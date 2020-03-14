08dcb9c7268d8ff9ad024d5c4d91be47c79fd986
vasile <vasica38@gmail.com>
2020-03-14 17:19:09 +0200

    good time

--- scripts/sheets.py
+++ scripts/sheets.py
@@ -8,10 +8,24 @@ class SheetReader:
         creds = ServiceAccountCredentials.from_json_keyfile_name('covid_MD-139a65d546ca.json', scope)
         self.client = gspread.authorize(creds)
 
-    def getAllSheetData(self, name):
+    def getAllSheetDataRaw(self, name):
         sheet = self.client.open(name).sheet1
         covid = sheet.get_all_records()
         return covid
 
-x = SheetReader()
-print(x.getAllSheetData("covid"))
+    def getAllSheetDataForPlot(self, name):
+        data = self.getAllSheetDataRaw(name)
+        for i in data:
+            i["data"] = i["data"] + " " + i["ora"]
+        return data
+
+# x = SheetReader()
+#
+# data = x.getAllSheetDataRaw("covid")
+#
+#
+#
+# print(data)
+
+
+
