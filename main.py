9db0a4dd13a9bd5a3a03f9a291e3ae7ff41356d7
EugeneKingF <47446543+EugeneKingF@users.noreply.github.com>
2020-03-15 20:22:47 +0200

    main.py
    adaugarea rutei /timeline

--- main.py
+++ main.py
@@ -4,19 +4,76 @@ import os, json, string
 import shutil, glob, datetime
 import sys
 import io
-
 import argparse
 
-from scripts.sheets import getData
+from scripts.sheets import SheetReader
+from scripts.timeline_table import extract_data
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'saaa'
 
 
+@app.route('/resetdata', methods=['GET'])
+def reset():    
+    x = SheetReader()
+    data = x.getAllSheetDataRaw("covid")
+    with open('data/minister.txt', 'w') as outfile:
+    	json.dump(data, outfile)
+    data = x.getAllSheetDataRaw("intrebari")
+    with open('data/intrebari.txt', 'w') as outfile:
+    	json.dump(data, outfile)
+    data = x.getAllSheetDataRaw("timeline")
+    with open('data/timeline.txt', 'w') as outfile:
+    	json.dump(data, outfile)
+    data = x.getAllSheetDataRaw("cuvinte")
+    with open('data/cuvinte.txt', 'w') as outfile:
+    	json.dump(data, outfile)
+    return 'done'
+
+def readData():
+	with open('data/minister.txt') as json_file:
+		mn = json.load(json_file)
+	data = {}
+	for k in mn[-1]:
+		data[k] = mn[-1][k]
+		data['y_'+k] = [i[k]  for i in mn]
+	data['zile'] = len({i.split('/2020')[0]:1 for i in data['y_data']})
+	return data
+
 @app.route('/', methods=['GET'])
 def index():
-    data = getData()
-    return render_template('index.html', data=data)
+	data  = readData()
+	return render_template('index3.html', data=data)
+
+@app.route('/intreaba', methods=['GET'])
+def ask():
+	data  = []#readData()
+	return render_template('ask.html', data=data)
+
+@app.route('/intrebari-frecvente', methods=['GET'])
+def qanda():
+	data  = []#readData()
+	return render_template('qanda.html', data=data)
+
+@app.route('/datelazi', methods=['GET'])
+def datelazi():
+	data  = []#readData()
+	return render_template('index3.html', data=data)
+
+@app.route('/land', methods=['GET'])
+def land():
+	data  = []#readData()
+	return render_template('land.html', data=data)
+
+@app.route('/timeline', methods=['GET'])
+def timeline():
+	data  = extract_data()
+	return render_template('includes/timeline.html', data=data)
+
+@app.route('/data', methods=['GET'])
+def data():
+	data  = readData()
+	return jsonify(data)
 
 def getA():
     parser = argparse.ArgumentParser(description='Test a network')
@@ -32,4 +89,4 @@ if __name__ == '__main__':
     if pars['live']:
         app.run(host='0.0.0.0', port=80, debug =True)
     else:
-        app.run(host='0.0.0.0', port=6001, debug =True)
+        app.run(host='localhost', port=6001, debug =True)
